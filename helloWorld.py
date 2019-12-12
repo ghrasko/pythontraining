7fcd34763368cf6f312f48a838a819028766b174
Gabor Hrasko <info@raskombt.com>
2019-12-12 18:48:57 +0100

    first

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,5 @@
 import sys
+
 import requests
 
 print(sys.version)
@@ -10,3 +11,6 @@ print('This is i: {0:06d} or {0:06.2f}'.format(i))
 
 r = requests.get("http://xaknak.hrasko.com")
 print(r.status_code)
+
+
+
