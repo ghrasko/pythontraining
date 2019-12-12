36395e4d0116da5e85fa14377dfe76116f766480
Gabor Hrasko <info@raskombt.com>
2019-12-12 18:31:34 +0100

    demo

--- helloWorld.py
+++ helloWorld.py
@@ -1,8 +1,12 @@
 import sys
+import requests
 
 print(sys.version)
 print(sys.executable)
 
+
 i = 2
 print('This is i: {0:06d} or {0:06.2f}'.format(i))
 
+r = requests.get("http://xaknak.hrasko.com")
+print(r.status_code)
